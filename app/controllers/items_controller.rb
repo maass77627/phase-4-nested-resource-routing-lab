f36d6768f949fbf5e9bea9427179987e050ed393
kelly_maass <kmaass77627@gmail.com>
2024-09-15 12:50:04 -0500

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
 
   def index
+    if params[:user_id]
+      user = User.find_by!(id: params[:user_id])
+      items = user.items
+    else
     items = Item.all
+    
+   
+    end
     render json: items, include: :user
   end
+
+  def show
+   item = Item.find_by!(id: params[:id])
+   render json: item, include: :user
+  end
+
+  def create
+   
+      user = User.find_by!(id: params[:user_id])
+   
+    items = user.items.create(item_params)
+    render json: items, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
 
 end
